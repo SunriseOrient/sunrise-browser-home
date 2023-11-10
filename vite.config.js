f4b99a4d30f8ddd84180f3284a79f2328b409d4b
Sun Rising <275050943@qq.com>
2023-11-10 17:54:43 +0800

    累计提交

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Icons from 'unplugin-icons/vite'
-import IconsResolver from 'unplugin-icons/resolver'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
+  build: {
+    outDir: "docs",
+  },
   plugins: [
     Vue(),
     AutoImport({
-      resolvers: [
-        ElementPlusResolver(),
-      ],
+      resolvers: [ElementPlusResolver()],
     }),
     Components({
       resolvers: [
         // 自动注册图标组件
         IconsResolver({
-          enabledCollections: ['ep'],
+          enabledCollections: ["ep"],
         }),
         ElementPlusResolver(),
       ],
@@ -27,4 +28,4 @@ export default defineConfig({
       autoInstall: true,
     }),
   ],
-})
+});
