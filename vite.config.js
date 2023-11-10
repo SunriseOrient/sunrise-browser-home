377a43996b62cba3cda31e6a5d246257f70d8d9c
Sun Rising <275050943@qq.com>
2023-11-10 21:01:17 +0800

    打包调整

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
-  base: "/sunrise-browser-home/",
+  base: "./",
   build: {
     outDir: "docs",
   },
